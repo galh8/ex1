5ba4b9102b96cd5fe3fb55d6b3ae33982c264230
Amir <halfonamir1@gmail.com>
2017-04-09 17:09:30 +0300

    Searcher

---
+++ ex1/SearchAlgorithmsLib/Searcher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SearchAlgorithmsLib
+{
+    public abstract class Searcher : ISearcher
+    {
+
+        private MyPriorityQueue<State> openList;
+        private int evaluatedNodes;
+
+        public Searcher()
+        {
+            openList = new MyPriorityQueue<State>();
+            evaluatedNodes = 0;
+        }
+
+        protected State popOpenList()
+        {
+            evaluatedNodes++;
+            return openList.poll();
+        }
+        public int OpenListSize
+        {
+            get { return openList.Count; }
+        }
+        public int getNumberOfNodeEvaluated()
+        {
+            return evaluatedNodes;
+        }
+        public abstract Solution search(ISearchable searchable);
+    }
+
+}
+
