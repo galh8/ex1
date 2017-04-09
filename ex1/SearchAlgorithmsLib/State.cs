e6dde51d0ad9ec43ea4f22984aed6b46bfd297c2
galh8 <galhuta@gmail.com>
2017-04-09 16:59:47 +0200

    state and bfs

---
+++ ex1/SearchAlgorithmsLib/State.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmsLib
+{
+    public class State
+    {
+        private string state;
+        private double cost;
+        private State cameFrom;
+
+        public State(string state)
+        {
+            this.state = state;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return state.Equals((obj as State).state);
+        }
+    }
+}
---
+++ ex1/SearchAlgorithmsLib/bfs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmsLib
+{
+    class Bfs : ISearchable
+    {
+
+    }
+
+    public override Bfs search(ISearchable searchable)
+    {
+        addToOpenList(searchable.getIntialState());
+        HashSet<State> closed = new HashSet<State>();
+        while (OpenListsSize > 0) {
+            State n = popOpenList();
+            closed.Add(n);
+            if (n.Equals(searchable.getIGoalLState()))
+                return backTrace();
+            List<State> succerssors = searchable.getAllPossibleStates(n);
+            foreach (State s in succerssors)
+            {
+                if (!closed.Contains(s) && !openContainers(s))
+                {
+                    addToOpenList(s);
+                }
+                else
+                {
+
+                }
+            }
+
+        }
+    }
+}
