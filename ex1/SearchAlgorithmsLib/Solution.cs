2da217aa1114b446829a71f44b85eda92bc29296
Amir <halfonamir1@gmail.com>
2017-04-10 14:17:53 +0300

    Solution!

--- ex1/SearchAlgorithmsLib/Solution.cs
+++ ex1/SearchAlgorithmsLib/Solution.cs
@@ -9,6 +9,30 @@ namespace SearchAlgorithmsLib
     class Solution<T>
     {
         private List<T> pathToGoal;
+        
+        public List<T> PathToGoal
+        {
+            get; set; 
+        }
 
+        public override string ToString()
+        {
+            StringBuilder pathToReturn = new StringBuilder();
+            foreach(T node in pathToGoal)
+            {
+                pathToReturn.Append(node.ToString());
+            }
+            return pathToReturn.ToString();
+        }
+
+        public void insertNode(T node)
+        {
+            pathToGoal.Add(node);
+        }
+
+        public int solutionLength()
+        {
+            return pathToGoal.Count();
+        }
     }
 }
