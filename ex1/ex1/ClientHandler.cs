32692c6a746f07e4becbec3b2af4440be199c715
Amir <halfonamir1@gmail.com>
2017-04-12 16:34:46 +0300

    MVC

---
+++ ex1/ex1/ClientHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ex1
+{
+    class ClientHandler : IClientHandler
+    {
+        private 
+
+        public void HandleClient(TcpClient client)
+        {
+            new Task(() =>
+            using (NetworkStream stream = client.GetStream())
+            using (StreamReader reader = new StreamReader(stream))
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                string commandLine = reader.ReadLine();
+
+            }
+
+            ).Start();
+        }
+    }
+}
---
+++ ex1/ex1/IController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SearchAlgorithmsLib;
+
+namespace ex1
+{
+    interface IController
+    {
+        void setModel(IModel m);
+        void setView(IClientHandler v);
+        void setSolution(Solution<T> s);
+        void calculate(Problem p);
+    }
+}
