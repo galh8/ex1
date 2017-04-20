ce7743d656d27a5c70371cbc918b14bbb7a4314c
Amir <halfonamir1@gmail.com>
2017-04-20 13:23:31 +0300

    Another try on close.. still have problems. check singleplayer commands...

--- ex1/ClientProject1/Client.cs
+++ ex1/ClientProject1/Client.cs
@@ -29,7 +29,11 @@ namespace ClientProject
             client.Connect(ep);
             Console.WriteLine("Client connected");
             bool isMultiplayer = false;
-
+            bool closeMultiplayer = false;
+            bool newInput = false;
+            bool inputStarted = false;
+            bool gotFeedback = false;
+            string clientCommand = "";
 
             using (NetworkStream stream = client.GetStream())
             using (StreamReader reader = new StreamReader(stream))
@@ -37,115 +41,117 @@ namespace ClientProject
             {
                 while (true)
                 {
-                    // Sending a command to server
-                    Console.Write("Please enter a command: ");
-                    string command = Console.ReadLine();
-                    if (command.Contains("start") || command.Contains("join"))
+                    Task multiPlayerReader = new Task(() =>
                     {
-                        isMultiplayer = true;
-                    }
-                    writer.WriteLine(command);
-                    writer.Flush();
-                    Console.WriteLine("{0}", command);
-
-                    //reading a reply from server
-                    while (true)
-                    {
-                        string feedback = reader.ReadLine();
-                        if (reader.Peek() == '@')
+                        while (!closeMultiplayer)
                         {
-                            feedback.TrimEnd('\n');
-                            break;
-                        }
-                        Console.WriteLine("{0}", feedback);
-                    }
-                    reader.ReadLine();
-
-                    if (isMultiplayer)
-                    {
-                        bool close = false;
-                        Task multiplayerReader = new Task(() =>
-                        {
-                            
-                            while (!close)
+                            while (true)
                             {
-                                
-                                while (!close)
+                                string feedback = reader.ReadLine();
+                                if (feedback.Contains("close"))
                                 {
-                                    string feedback = reader.ReadLine();
-                                    if (feedback.Contains("close_connection"))
-                                    {
-                                        Console.WriteLine("Other client notified");
-                                        close = true;
-                                        break;
-                                    }
-                                    if (reader.Peek() == '@')
-                                    {
-                                        feedback.TrimEnd('\n');
-                                        break;
-                                    }
-                                    Console.WriteLine("{0}", feedback);
-
-                                }
-                                
-                                reader.ReadLine();
-                            }
-                            Console.WriteLine("Other while loop ended");
-                            
-                        });
-
-
-
-                        
-                        Task multiplayerWriter = new Task(() =>
-                        {
-                            while (!close)
-                            {
-                                Console.Write("Please enter a multiplayer command: ");
-                                try
-                                {
-                                    string multiplayerCommand = Console.ReadLine();
-                                    writer.WriteLine(multiplayerCommand);
-                                    writer.Flush();
-                                    if (multiplayerCommand.Contains("close"))
-                                    {
-                                        Console.WriteLine("Client stops the thread");
-                                        close = true;
-                                        break;
-                                    }
-                                }catch (SocketException s)
-                                {
+                                    //Console.WriteLine("Other client notified");
+                                    closeMultiplayer = true;
+                                    gotFeedback = true;
                                     break;
                                 }
-                                
-                               // Console.WriteLine("{0}", multiplayerCommand);
+                                if (reader.Peek() == '@')
+                                {
+                                    feedback.TrimEnd('\n');
+                                    break;
+                                }
+                                Console.WriteLine("{0}", feedback);
                             }
-                            Console.WriteLine("MYself while stopped");
+                            reader.ReadLine();
+                            gotFeedback = true;
+                        }
+                        gotFeedback = true;
+                        Console.WriteLine("reader thread finished");
+                    });
 
-                        });
+                    Task multiPlayerWriter = new Task(() =>
+                    {
+                        while (!closeMultiplayer)
+                        {
+                            // Console.Write("Please enter a multiplayer command: ");
+                            //     string multiplayerCommand = Console.ReadLine();
+                            while (newInput)
+                            {
+                                newInput = false;
+                                writer.WriteLine(clientCommand);
+                                writer.Flush();
+                                if (clientCommand.Contains("close"))
+                                {
+                                    Console.WriteLine("Client stops the thread");
+                                    closeMultiplayer = true;
+                                    break;
+                                }
+                            }
+                        }
+                        newInput = false;
+                        Console.WriteLine("MYself while stopped");
+                        //multiPlayerReader.Dispose();
+                        //Thread.CurrentThread.Abort();
+                    });
 
-                        multiplayerReader.Start();
-                        multiplayerWriter.Start();
-
-                        multiplayerReader.Wait();
-                        
-                        //multiplayerWriter.di
-                        //multiplayerWriter.Wait();
-
-                        //multiplayerWriter;
-                        Console.WriteLine("After Writer wait");
-
+                    Task consoleReadTask = new Task(() =>
+                    {
+                        while (true)
+                        {
+                            //Sending a command to server
+                            Console.Write("Please enter a command: ");
+                            clientCommand = Console.ReadLine();
+                            newInput = true;
+                            if (clientCommand.Contains("start") || clientCommand.Contains("join"))
+                            {
+                                closeMultiplayer = false;
+                                Console.WriteLine("STARTING MULTIPLAYER!");
+                                isMultiplayer = true;
+                                multiPlayerWriter.Start();
+                                Thread.Sleep(5);
+                                multiPlayerReader.Start();
+                                while (!gotFeedback) { }
+                                gotFeedback = false;
+                                Console.WriteLine("AFTERRRRRRR BLOCK");
+   
+                            }
+                            if (clientCommand.Contains("close"))
+                                closeMultiplayer = true;
+                        }
+                    });
+                    if (!inputStarted)
+                    {
+                        inputStarted = true;
+                        consoleReadTask.Start();
                     }
-                    
-                }
-                client.Close();
-             } 
+                    //if (isMultiplayer)
+                    //{
+                    //    Console.WriteLine("Before writer wait!");
+
+                    //    multiPlayerReader.Wait();
+                    //   Console.WriteLine("After reader wait!");
+                    //    multiPlayerWriter.Wait();
+                    //    //Console.WriteLine("After WAITSSSSS");
+                    //    isMultiplayer = false;
+                    //}
+                    if (isMultiplayer)
+                    {
+                        while (!closeMultiplayer) { }
+                        //multiPlayerReader.Wait();
+                        //multiPlayerWriter.Wait();
+                        Console.WriteLine("After WAITSSSSS");
+                        isMultiplayer = false;
+                    }
+                }          
+             }
+                //client.Close();
+         } 
 
                 //client.Close();
-            } // end of the class
+      } // end of the class
 
 
-        }
-    }
+ }
+    
 
 
